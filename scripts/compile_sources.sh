472685c5887840b150272ba1d210c8bcaf24708c
Gabriel R. Abreu <grsabreu@gmail.com>
2017-07-29 22:43:34 -0300

    Remove unnecessary scripts

--- scripts/compile_sources.sh
+++
@@ -1,9 +0,0 @@
-echo "> Transpiling Javascript to ES2015"
-echo ""
-babel --plugins "transform-runtime" lib --ignore __tests__ --out-dir ./dist
-cd dist
-browserify --debug -t [ exposify --expose [ --react React --react-dom ReactDOM] ] \
-  -p [ css-modulesify -o styles.css ] ./browser_exposer.js > ./browser.js
-cat ./browser.js | uglifyjs -c > ./browser.min.js
-echo ""
-echo "> Complete transpiling Javascript to ES2015"
--- scripts/copy_bundle_to_example.sh
+++
@@ -1,9 +0,0 @@
-echo "> Copying bundle to example/"
-
-rm example/browser.js
-cp dist/browser.js example/browser.js
-
-rm example/styles.css
-cp dist/styles.css ./example
-
-echo "> Copying bundle complete."
